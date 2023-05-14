70eed71f089e0412e2fcd999aa7be4fb1d83a85f
appuser <appuser@journey2-7d499d958c-hw8wv>
2023-05-14 18:54:11 +0000

    starter project

---
+++ src/main/java/com/codecool/usermanagement/Application.java
@@ -0,0 +1,69 @@
+package com.codecool.usermanagement;
+
+public class Application {
+
+    public static void main(String[] args) {
+        String[] names = { "John", "Alexa", "Kyle" };
+        int[] ages = { 20, 25, 40 };
+        Gender[] genders = { Gender.Male, Gender.Female, Gender.Male };
+    }
+
+    // Summary
+
+    private static String summarize(String name, int age, Gender gender) {
+        // TODO: implement
+        return null;
+    }
+
+    private static String[] summarize(String[] names, int[] ages, Gender[] genders) {
+        // TODO: implement
+        return null;
+    }
+
+    private static void displaySummary(String summary) {
+        // TODO: implement
+    }
+
+    private static void displaySummary(String[] summaries) {
+        // TODO: implement
+    }
+
+    //
+
+    // Update age
+
+    private static void updateAge(int index, int[] ages, int newAge) {
+        // TODO: implement
+    }
+
+    //
+
+    // User input
+
+    private static String getInput() {
+        // TODO: implement
+        return null;
+    }
+
+    private static int parseInput(String input) {
+        // TODO: implement
+        return 0;
+    }
+
+    private static int askForIndex(int max) {
+        // TODO: implement
+        return 0;
+    }
+
+    private static boolean askIfUpdateAge() {
+        // TODO: implement
+        return false;
+    }
+
+    private static int askForNewAge() {
+        // TODO: implement
+        return 0;
+    }
+
+    //
+}
---
+++ src/main/java/com/codecool/usermanagement/Gender.java
@@ -0,0 +1,6 @@
+package com.codecool.usermanagement;
+
+public enum Gender {
+    Male,
+    Female
+}
